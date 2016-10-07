4fa8ffdef41ae18d32af4f30decea59813b0426a
Tine <tine@toll.de>
2016-10-07 09:28:04 +0200

    Erstes Design

---
+++ routing/src/model/RoutingGraph.java
@@ -0,0 +1,27 @@
+package model;
+
+/**
+ *
+ * @author OstBot, PartyRazorHolland, Seegelkernspaltung
+ */
+public class RoutingGraph {
+
+    private final int[][] distances;
+    private final String[] nodeNames;
+
+    public RoutingGraph(int[][] distances, String[] nodeNames) {
+        this.distances = distances;
+        this.nodeNames = nodeNames;
+    }
+
+    public int[][] getDistances() {
+        return distances;
+    }
+
+    public String[] getNodeNames() {
+        return nodeNames;
+    }
+
+   
+
+}
