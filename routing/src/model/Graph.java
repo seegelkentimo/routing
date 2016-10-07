c667d7a1b7fe55dca5368123e6933078af60fa2f
Timo <timo.seegelken@gmx.de>
2016-10-07 09:20:31 +0200

    Erstes Design

--- routing/src/model/Graph.java
+++ routing/src/model/Graph.java
@@ -5,11 +5,18 @@ package model;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author user
  */
 public class Graph {
-    
+
+    private int[][] distances;
+    private String[] nodeNames;
+
+    public Graph(int[][] distances, String[] nodeNames) {
+        this.distances = distances;
+        this.nodeNames = nodeNames;
+    }
+
 }
